8a02c3fd52269e7ef361d3ce9d7338a2042f3a99
Maedemdi <maedemajidi.381@gmail.com>
2025-07-20 03:57:42 +0330

    Added the ability to scrape the news

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -1,12 +1,10 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Item, Field
+from itemloaders.processors import MapCompose, TakeFirst
 
 
-class ScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ScraperItem(Item):
+    title = Field(output_processor = TakeFirst())
+    text = Field()
+    # tags = Field()
+    created_at = Field(output_processor = TakeFirst())
+    # writer = Field(output_processor = TakeFirst())
--- scraper/scraper/spiders/zoomit_spider.py
+++ scraper/scraper/spiders/zoomit_spider.py
@@ -1,10 +1,44 @@
 import scrapy
+from scraper.items import ScraperItem
+from scrapy.loader import ItemLoader
 
 
 class ZoomitSpiderSpider(scrapy.Spider):
     name = "zoomit_spider"
     allowed_domains = ["zoomit.ir"]
-    start_urls = ["https://zoomit.ir"]
+    start_urls = ["https://www.zoomit.ir/"]
+
 
     def parse(self, response):
-        pass
+        urls = response.css("a[href^='https://www.zoomit.ir/']::attr(href)").getall()
+
+        for url in urls:
+            yield scrapy.Request(
+                url= url,
+                meta= {
+                    "playwright": True,
+                },
+                callback= self.parse_details
+            )
+
+
+    def parse_details(self, response):
+        news = response.css("main.bg-background2.border-grey6.relative.pb-12")
+        title = news.css("div.fjdwzl h1.sc-9996cfc-0::text").get()
+        text_selectors = news.css(".sc-481293f7-1.jrhnOU .sc-9996cfc-0::text").getall()
+        created_at = news.css("span.sc-9996cfc-0.inKOvi.fa::text").get()
+        tags = news.css("a span.sc-9996cfc-0.NawFH::text").getall()
+        writer = news.css("div.sc-a11b1542-0.GGjpx span::text").get()
+
+        content = ''
+        for text in text_selectors:
+            content += text
+
+        if title is not None:
+            yield {
+                    "title": title,
+                    "text": content,
+                    "created_at": created_at,
+                    "tags": tags,
+                    "writer": writer
+                }
